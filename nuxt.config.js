24f4ba0cf1e913239e7776c1a6dde45ffa24e052
EMRE ZAVAR <zavaremre@gmail.com>
2022-03-09 11:51:50 +0300

    server settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,10 +16,7 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-  server: {
-    port: '3000',
-    host: '0.0.0.0',
-  },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/css/main.css'],
 
